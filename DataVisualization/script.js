2db15a1cf6e4044e0e1f9e794aadc91eb714f98a
maybe0000 <saramarkic99@gmail.com>
2024-10-12 21:06:51 +0200

    Creating x and y axis

--- DataVisualization/script.js
+++ DataVisualization/script.js
@@ -1,3 +1,5 @@
+console.log("script.js loaded");
+
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
             .then(response => response.json())
             .then(data => document.getElementById('bar-chart').innerHTML = JSON.stringify(data));
