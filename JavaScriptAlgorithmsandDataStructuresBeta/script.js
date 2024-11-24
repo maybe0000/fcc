c1134ba4af424b018912a23bd1a491dd6ef89ac6
maybe0000 <saramarkic99@gmail.com>
2024-11-24 13:17:44 +0100

    Testing script.js

--- JavaScriptAlgorithmsandDataStructuresBeta/script.js
+++ JavaScriptAlgorithmsandDataStructuresBeta/script.js
@@ -1,1 +1,11 @@
-// Initial commit
+const result = document.getElementById("result");
+const checkButton = document.getElementById("check-btn");
+const textInput = document.getElementById("text-input");
+
+const printResult = (event) => {
+    event.preventDefault();
+    const input = textInput.value;
+    result.innerText = `${input}`;
+};
+
+checkButton.addEventListener("click",  printResult);
