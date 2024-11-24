417ae457de86ac46a7b9a6a39feecde7e362e801
maybe0000 <saramarkic99@gmail.com>
2024-11-24 18:31:32 +0100

    Added palindrome checker function

--- JavaScriptAlgorithmsandDataStructuresBeta/script.js
+++ JavaScriptAlgorithmsandDataStructuresBeta/script.js
@@ -2,10 +2,34 @@ const result = document.getElementById("result");
 const checkButton = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 
+const checkIfPalindrome = (input) => {
+    const cleanInput = input.replace(/[^a-zA-Z0-9]/g, '').toUpperCase();
+
+    if (cleanInput === cleanInput.split('').reverse().join('')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
 const printResult = (event) => {
     event.preventDefault();
     const input = textInput.value;
-    result.innerText = `${input}`;
+    if (input === '') {
+        alert('Please input a value');
+        return;
+    }
+    if(checkIfPalindrome(input)) {
+        result.innerText = `${input} is a palindrome.`;
+    }
+    else {
+        result.innerText = `${input} is not a palindrome.`;
+    }
 };
 
 checkButton.addEventListener("click",  printResult);
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
