884756fc73617564aa07b9332620519001290933
maybe0000 <saramarkic99@gmail.com>
2024-12-24 21:40:53 +0100

    Printing the register

--- JavaScriptAlgorithmsandDataStructuresBeta/CashRegister/script.js
+++ JavaScriptAlgorithmsandDataStructuresBeta/CashRegister/script.js
@@ -1,3 +1,9 @@
+const cashInput = document.getElementById('cash');
+const changeDue = document.getElementById('change-due');
+const purchaseBtn = document.getElementById('purchase-btn');
+const cashInDrawer = document.getElementById('cash-in-drawer');
+const priceOfItem = document.getElementById('price-of-item');
+
 let price = 1.87;
 let cid = [
   ['PENNY', 1.01],
@@ -11,9 +17,30 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
-const cashInput = document.getElementById('cash');
-const changeDue = document.getElementById('change-due');
-const purchaseBtn = document.getElementById('purchase-btn');
+const titles = {
+    PENNY: 'Pennies',
+    NICKEL: 'Nickels',
+    DIME: 'Dimes',
+    QUARTER: 'Quarters',
+    ONE: 'Ones',
+    FIVE: 'Fives',
+    TEN: 'Tens',
+    TWENTY: 'Twenties',
+    "ONE HUNDRED": 'Hundreds'
+};
+
+let cidSum = cid.reduce((sum,i) => sum+i[1],0);
+priceOfItem.textContent = `Total: $${price}`;
+
+const cidPrint = () => {
+    let printedRegister = '';
+    for(let i = 0; i< cid.length; i++) {
+        printedRegister += `${titles[cid[i][0]]}: $${cid[i][1]}<br>`;
+    }
+    return printedRegister;
+}
+
+cashInDrawer.innerHTML = `<b>Cash in drawer:</b><br><br>`+cidPrint();
 
 const clearInput = () => {
     cashInput.value='';
@@ -22,4 +49,10 @@ const clearInput = () => {
 purchaseBtn.addEventListener('click',() => {
     const floatInput = parseFloat(cashInput.value);
     if(floatInput < price) {
-        alert("Customer does not have enough money to purchase the it
+        alert("Customer does not have enough money to purchase the item");
+        clearInput();
+    } else if (floatInput === price) {
+        changeDue.textContent = 'No change due - customer paid with exact cash';
+        clearInput();
+    }
+});
