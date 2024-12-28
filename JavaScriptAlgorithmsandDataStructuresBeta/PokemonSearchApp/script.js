de267d121bc9b5f59e678d8e5bd704701753c4bf
maybe0000 <saramarkic99@gmail.com>
2024-12-28 20:35:51 +0100

    Adding test image

--- JavaScriptAlgorithmsandDataStructuresBeta/PokemonSearchApp/script.js
+++ JavaScriptAlgorithmsandDataStructuresBeta/PokemonSearchApp/script.js
@@ -2,7 +2,19 @@ const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const pokemonContainer = document.getElementById('pokemon-container');
 const imageContainer = document.getElementById('image-container');
+const pokemonImg = document.getElementById('pokemon-img');
 const statsContainer = document.getElementById('stats-container');
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonId = document.getElementById('pokemon-id');
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const types = document.getElementById('types');
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
 
 let pokemonData = [];
 
@@ -29,28 +41,44 @@ const checkInput = (input) => {
 const clearScreen = () => {
     searchInput.value = '';
     imageContainer.innerHTML = '';
+}    
+
+const updateDisplay = (pokemon) => {
+    pokemonImg.innerHTML = '';
+    pokemonName.textContent = pokemon.name.toUpperCase();
+    pokemonId.textContent = '#'+pokemon.id;
+    const img = document.createElement('img');
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/673.png`;
+    img.alt = `${pokemon.name.toLowerCase()}-img`; 
+    img.classList.add('createdImg');
+    pokemonImg.appendChild(img);
 };
+
 const displayPokemon = () => {
     if (pokemonData) {
-        let userInput = checkInput(searchInput.value);
-        switch (userInput) {
+        let userInput = searchInput.value;
+        let userInputCheck = checkInput(userInput);
+        let pokemon = '';
+        switch (userInputCheck) {
             case 'invalid':
                 alert("Pokémon not found");
                 clearScreen();
                 break;
             case 'id':
-                imageContainer.innerHTML = 'ID ' + userInput;
+                pokemon = pokemonData.find(i => i.id === parseInt(userInput));
+                updateDisplay(pokemon);
                 break;
             case 'name':
-                imageContainer.innerHTML = 'NAME:' + userInput;
+                pokemon = pokemonData.find(i => i.name.toUpperCase() === userInput.toUpperCase());
+                updateDisplay(pokemon);
                 break;         
         }
     }
 };
 
 searchButton.addEventListener('click', displayPokemon);
-searchButton.addEventListener('keydown', (e) => {
+searchInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         displayPokemon();
     }
-})
+});
