de084eaf0584382f3deb16778d78ee4b6a45677a
maybe0000 <saramarkic99@gmail.com>
2025-01-17 18:04:29 +0700

    Serve a HTML file as a response

--- BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
+++ BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let app = express();
 console.log("Hello world");
+// app.get('/', function(req, res) {
+//     res.send('Hello Express');
+// })
 app.get('/', function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 
