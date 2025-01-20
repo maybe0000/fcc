b20745196160a3830aa4f3247ed3d1f811236209
saram m <saramarkic99@gmail.com>
2025-01-20 14:19:24 +0000

    Chaining middleware functions

--- BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
+++ BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
@@ -14,12 +14,17 @@ app.get('/', function(req, res) {
 })
 app.use('/public',express.static(__dirname + '/public'));
 app.get('/json', function(req, res) {
-    let msg = "Hello json"
+    let msg = "Hello json";
     //console.log(process.env.MESSAGE_STYLE)
-    let message = process.env.MESSAGE_STYLE == 'uppercase' ? msg.toUpperCase() : msg
-    res.json({"message": message})
+    let message = process.env.MESSAGE_STYLE == 'uppercase' ? msg.toUpperCase() : msg;
+    res.json({"message": message});
 })
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({"time": req.time});
+})
 
 
 
