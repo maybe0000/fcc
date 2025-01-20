d47612bf5baefc3de8462e4ca0505bdcb0336366
saram m <saramarkic99@gmail.com>
2025-01-20 14:07:02 +0000

    Middleware logger

--- BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
+++ BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
@@ -5,16 +5,20 @@ console.log("Hello world");
 // app.get('/', function(req, res) {
 //     res.send('Hello Express');
 // })
+app.use(function(req, res, next) {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    next();
+})
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 })
 app.use('/public',express.static(__dirname + '/public'));
 app.get('/json', function(req, res) {
     let msg = "Hello json"
+    //console.log(process.env.MESSAGE_STYLE)
     let message = process.env.MESSAGE_STYLE == 'uppercase' ? msg.toUpperCase() : msg
     res.json({"message": message})
 })
-
 
 
 
