3afe35fb46d597a7c176474059dbeb002661e942
maybe0000 <saramarkic99@gmail.com>
2025-01-21 18:32:21 +0700

    Comment + get query param input from the client

--- BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
+++ BackEndDevelopmentAndAPIs/BasicNodeAndExpress/boilerplate-express/myApp.js
@@ -32,7 +32,9 @@ app.get('/:word/echo', function(req, res) {
 app.get('/name', function(req, res) {
     res.json({name: req.query.first + ' ' + req.query.last});
 })
-
+// app.route('/name').get(function(req, res) {
+//     res.json({name: req.query.first + ' ' + req.query.last});
+// }).post(handler)
 
 
 
