0a2faab3554749fab41a2fd357a3af5d61d49448
maybe0000 <saramarkic99@gmail.com>
2025-01-28 20:35:42 +0700

    Use model.findOne() to return a single matching document from your database

--- BackEndDevelopmentAndAPIs/MongoDBAndMongoose/boilerplate-mongomongoose/myApp.js
+++ BackEndDevelopmentAndAPIs/MongoDBAndMongoose/boilerplate-mongomongoose/myApp.js
@@ -46,7 +46,14 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, data) {
+    if (err) {
+      return done(err);
+    }
+    else {
+      return done(null, data);
+    }
+  })
 };
 
 const findPersonById = (personId, done) => {
