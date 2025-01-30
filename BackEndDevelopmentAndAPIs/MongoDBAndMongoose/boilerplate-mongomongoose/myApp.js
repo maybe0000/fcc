5dbe70d4e4830435fddf1c194e1a271c3cf81df8
maybe0000 <saramarkic99@gmail.com>
2025-01-30 16:47:01 +0700

    Perform classic updates by running find, edit, then save

--- BackEndDevelopmentAndAPIs/MongoDBAndMongoose/boilerplate-mongomongoose/myApp.js
+++ BackEndDevelopmentAndAPIs/MongoDBAndMongoose/boilerplate-mongomongoose/myApp.js
@@ -70,7 +70,22 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  findPersonById(personId, (err, person) => {
+    if (err) {
+      return done(err);
+    }
+
+    person.favoriteFoods.push(foodToAdd);
+
+    person.save((err, updatedPerson) => {
+      if (err) {
+        return done(err);
+      }
+      else {
+        done(null, updatedPerson);
+      }
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
