ee752787c84e34a528eb00d5ba5ee5b4b71f8012
maybe0000 <saramarkic99@gmail.com>
2025-02-04 21:17:09 +0700

    Fix of the API date function

--- BackEndDevelopmentAndAPIs/01_TimestampMicroservice/index.js
+++ BackEndDevelopmentAndAPIs/01_TimestampMicroservice/index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,26 +18,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
 // API date
-app.get('/api/:date?', function(req, res, next) {
+app.get('/api/:date?', function (req, res) {
   let dateAPI = req.params.date;
   let date = Number(dateAPI);
 
-  if(!dateAPI) {
+  if (!dateAPI) {
     res.json({
       unix: new Date().getTime(),
       utc: new Date().toUTCString()
     })
   }
-  else if(!isNaN(date) && date > 0) {
-  res.json({
+  else if (!isNaN(date) && date > 0) {
+    res.json({
       unix: date,
       utc: new Date(date).toUTCString()
     });
@@ -45,7 +43,7 @@ app.get('/api/:date?', function(req, res, next) {
   else {
     unixDate = new Date(dateAPI).getTime();
     utcDate = new Date(dateAPI).toUTCString();
-    if(!unixDate) {
+    if (!unixDate) {
       return res.json({
         error: "Invalid Date"
       });
@@ -56,10 +54,8 @@ app.get('/api/:date?', function(req, res, next) {
       })
     }
   }
-  next();
-}, function(req, res) {
-  res.json({ "unix": req.unix, "utc": req.utc});
-})
+}
+)
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
