2b4d34e5bf1474290f5a788cb609a5d300944a56
maybe0000 <saramarkic99@gmail.com>
2025-03-03 21:37:54 +0700

    File Metadata Microservice

--- BackEndDevelopmentAndAPIs/05_FileMetadataMicroservice/index.js
+++ BackEndDevelopmentAndAPIs/05_FileMetadataMicroservice/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +12,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // console.log(req)
+  const { originalname, mimetype, size } = req.file
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+})
 
 
 
